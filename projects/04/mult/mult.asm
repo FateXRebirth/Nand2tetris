8072a59653a0bbd15fe5c1a0e2bab7784accbf47
Kevin Peng <FateXRebirth@gmail.com>
2018-01-05 09:03:57 +0800

    Project 4 Homework

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// Initialization
+@sum
+M=0
+@R2
+M=0
+
+// LOOP
+(LOOP)
+	@R1
+	D=M
+	@END
+	D;JEQ
+	@R0
+	D=M
+	@sum
+	M=D+M
+	@R1
+	M=M-1
+	@LOOP
+	0;JMP
+	
+// END
+(END)
+	@sum
+	D=M
+	@R2
+	M=D
+	@END
+	0;JMP
+
