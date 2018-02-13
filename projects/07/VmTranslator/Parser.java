cd404de5532e49666a1d7737ccc7f8cb3274cd72
Kevin Peng <FateXRebirth@gmail.com>
2018-02-13 20:26:04 +0800

    VM Translator Design ( Parser )

--- projects/07/VmTranslator/Parser.java
+++ projects/07/VmTranslator/Parser.java
@@ -2,50 +2,49 @@ import java.io.*;
 
 public class Parser {
 
-	private static FileReader fileReader;
+    private static FileReader fileReader;
     private static BufferedReader bufferReader;
     private static String hasNext;
     private static String currentCommand;
-	private static String type;
-	private static String arg1;
-	private static int arg2;
+    private static String type;
+    private static String arg1;
+    private static int arg2;
 
-	public Parser(String input) {
+    public Parser(String input) {
         try {
             fileReader = new FileReader(input);
             bufferReader = new BufferedReader(fileReader);
         } catch (IOException e) {
-			e.printStackTrace();
-		}
+            e.printStackTrace();
+        }
     }
 
-    public static boolean hasMoreCommands() {
-        boolean has = true;
+    public static boolean HasMoreCommands() {
         try {
             hasNext = bufferReader.readLine();
             if( hasNext == null) {
                 bufferReader.close();
                 fileReader.close();
-                has = false;
+                return false;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }    
-        return has;
+        return true;
     }
 
-    public static void advance() {
+    public static void Advance() {
         currentCommand = hasNext;
     }
 
-    public static String commandType() {
+    public static String CommandType() {
         if (currentCommand.startsWith("//") || currentCommand.isEmpty()) {
             return "NULL";
         } else {
             if (currentCommand.indexOf("push") !=- 1) {
                 return "C_PUSH";
             }
-          	else if (currentCommand.indexOf("pop") != -1) {
+            else if (currentCommand.indexOf("pop") != -1) {
                 return "C_POP";
             }
             else if (currentCommand.indexOf("label") != -1) {
@@ -70,17 +69,36 @@ public class Parser {
                 return "C_ARITHMETIC";        
             }
         }
+    
+}
+    public static String Type() {
+        return type;
     }
 
-    public static String arg1() {
-        return "";
+    public static String Arg1() {
+        return arg1;
     }
 
-    public static int arg2() {
-        return 0;
+    public static int Arg2() {
+        return arg2;
     }
 
-    public static void handle() {
-      
+    public static void Handle() {
+      String[] lex = currentCommand.split(" ");
+      if(lex.length == 1) {
+        type = lex[0];
+      } else if (lex.length == 2) {
+        type = lex[0];
+        arg1 = lex[1];
+      } else {
+        type = lex[0];
+        arg1 = lex[1];
+        arg2 = Integer.valueOf(lex[2]);
+      }
+    }
+
+    // Debugger 
+    public static String GetCurrentCommand() {
+        return currentCommand;
     }
 }
