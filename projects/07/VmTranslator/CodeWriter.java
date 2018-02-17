ca3763d8fcf2adf41825fdc1a72fa20194dfcbb5
Kevin Peng <FateXRebirth@gmail.com>
2018-02-18 07:56:44 +0800

    VM Translator Design ( CodeWriter )

--- projects/07/VmTranslator/CodeWriter.java
+++ projects/07/VmTranslator/CodeWriter.java
@@ -21,8 +21,72 @@ public class CodeWriter {
 			code.add("@SP");
 			code.add("A=M");
 			code.add("D=M");
+			code.add("M=0");
 			code.add("@"+ String.valueOf(index));
 			code.add("M=D");
+		}
+
+		public void Add_Sub_And_Or_Command(String type) {
+			code.add("@SP");
+			code.add("M=M-1");
+			code.add("@SP");
+			code.add("A=M");
+			code.add("D=M");
+			code.add("@SP");
+			code.add("M=M-1");
+			code.add("@SP");
+			code.add("A=M");
+			code.add("A=M");
+			switch(type) {
+				case "Add":
+					code.add("D=A+D");
+					break;
+				case "Sub":
+					code.add("D=A-D");
+					break;
+				case "And":
+					code.add("D=D&A");
+					break;
+				case "Or":
+					code.add("D=D|A");
+					break;
+				default:
+					System.out.println("Add_Sub_And_Or_Command Error");
+					break;
+			}
+			code.add("@SP");
+			code.add("A=M");
+			code.add("M=D");
+			code.add("@SP");
+			code.add("M=M+1");
+		}
+
+		public void Neg_Not_Command(String type) {
+			code.add("@SP");
+			code.add("M=M-1");
+			code.add("@SP");
+			code.add("A=M");
+			code.add("D=M");
+			switch(type) {
+				case "Not":
+					code.add("D=!D");
+					break;
+				case "Neg":
+					code.add("D=-D");
+					break;
+				default:
+					System.out.println("Neg_Not_Command Error");
+					break;
+			}
+			code.add("@SP");
+			code.add("A=M");
+			code.add("M=D");
+			code.add("@SP");
+			code.add("M=M+1");
+		}
+
+		public void Compare_Command(String type) {
+
 		}
 	}
 
@@ -44,8 +108,40 @@ public class CodeWriter {
             ex.printStackTrace();
         }
 	}
-	
+
 	public static void writeArithmetic(String command) {
+		switch(command) {
+			case "add":
+				codeGenerator.Add_Sub_And_Or_Command("Add");
+				break;
+			case "sub":
+				codeGenerator.Add_Sub_And_Or_Command("Sub");
+				break;
+			case "neg":
+				codeGenerator.Neg_Not_Command("Neg");
+				break;
+			case "eq":
+				codeGenerator.Compare_Command("Eq");
+				break;
+			case "gt":
+				codeGenerator.Compare_Command("Gt");
+				break;
+			case "lt":
+				codeGenerator.Compare_Command("Lt");
+				break;
+			case "and":
+				codeGenerator.Add_Sub_And_Or_Command("And");
+				break;
+			case "or":
+				codeGenerator.Add_Sub_And_Or_Command("Or");
+				break;
+			case "not":
+				codeGenerator.Neg_Not_Command("Not");
+				break;
+			default:
+				System.out.println("writeArithmetic Error");
+				break;
+		}
 		write();
 	}
 
@@ -58,7 +154,7 @@ public class CodeWriter {
 				codeGenerator.Pop_Command(segment, index);
 				break;
 			default:
-				System.out.println("Invalid type");
+				System.out.println("writePushPop Error");
 				break;
 		}
 		write();
