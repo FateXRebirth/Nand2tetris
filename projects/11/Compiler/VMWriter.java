c8d8f598a4a1e39172d3d46479de915d81ba84f1
FateXRebirth <FateXRebirth@gmail.com>
2021-09-13 18:41:38 +0800

    Compiler - VMWriter Cont.

--- projects/11/Compiler/VMWriter.java
+++ projects/11/Compiler/VMWriter.java
@@ -23,39 +23,78 @@ public class VMWriter {
         }
     }
 
-    public void writePush() {
-
+    public void write(String code) {
+        try {
+            bufferedWriter.write(code);
+            bufferedWriter.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void writePop() {
-        
+    public void writePush(String segment, int index) {
+        write(String.format("push %s %s", segment, index));
     }
 
-    public void writeArithmetic() {
-        
+    public void writePop(String segment, int index) {
+        write(String.format("pop %s %s", segment, index));
     }
 
-    public void writeLabel() {
-        
+    public void writeArithmetic(String command) {
+        switch(command) {
+            case "ADD":
+                write("add");
+                break;
+            case "SUB":
+                write("sub");
+                break;
+            case "NEG":
+                write("neg");
+                break;
+            case "EQ":
+                write("eq");
+                break;
+            case "GT":
+                write("gt");
+                break;
+            case "LT":
+                write("lt");
+                break;
+            case "AND":
+                write("and");
+                break;
+            case "OR":
+                write("or");
+                break;
+            case "NOT":
+                write("not");
+                break;
+            default:
+                throw new NoSuchFieldError("no such command");
+        }
     }
 
-    public void writeGoto() {
-        
+    public void writeLabel(String label) {
+        write(String.format("label %s", label));
     }
 
-    public void writeIf() {
-        
+    public void writeGoto(String label) {
+        write(String.format("goto %s", label));
     }
 
-    public void writeCall() {
-        
+    public void writeIf(String label) {
+        write(String.format("if-goto %s", label));
     }
 
-    public void writeFunction() {
-        
+    public void writeCall(String name, int arguments) {
+        write(String.format("call %s %s", name, arguments));
+    }
+
+    public void writeFunction(String name, int locals) {
+        write(String.format("function %s %s", name, locals));
     }
 
     public void writeReturn() {
-        
+        write("return");
     }
 }
